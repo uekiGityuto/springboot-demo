3f222fbc67f3b7b635ef2d810203822810d21c39
ueki-yuto <ueki-yuto@dmm.com>
2022-06-15 22:44:19 +0900

    エンドポイント追加

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -9,16 +9,21 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping(value = "/")
-public class HelloController {
+public class DemoController {
 
-    @GetMapping("hello")
-    public Mono<String> get() {
+    @GetMapping("heavy")
+    public Mono<String> getHeavyMsg() {
         try {
             // Graceful Shutdownの挙動確認のために時間のかかる処理を再現
             TimeUnit.SECONDS.sleep(10);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return Mono.just("Hello!!");
+        return Mono.just("Heavy processing completed!!");
+    }
+
+    @GetMapping("light")
+    public Mono<String> getLightMsg() {
+        return Mono.just("light processing completed!!");
     }
 }
